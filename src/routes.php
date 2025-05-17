84477c6ca0e9a36305d40798e07608ed23e07aa0
mainbotan <ned.serafim@gmail.com>
2025-05-17 21:13:05 +0300

    делаем грязюку с роутингом

---
+++ src/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+// Конфиг маршрутов
+
+return [
+    [
+        'path' => '/artist/{id}/getInfo',
+        'method' => 'GET',
+        'handler' => ''
+    ],
+    [
+        'path' => '/artist/{id}/getTopTracks',
+        'method' => 'GET',
+        'handler' => ''
+    ]
+];

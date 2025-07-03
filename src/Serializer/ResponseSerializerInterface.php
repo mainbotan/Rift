feab856d55fa362fa388ecf5651a7a895f57eb82
mainbotan <ned.serafim@gmail.com>
2025-07-03 04:37:07 +0300

    rename Serializer to ResponseEmitter

--- src/Serializer/ResponseSerializerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Rift\Core\Serializer;
-
-use Rift\Core\Contracts\OperationOutcome;
-
-interface ResponseSerializerInterface {
-    public function serialize(OperationOutcome $o): string;
-}

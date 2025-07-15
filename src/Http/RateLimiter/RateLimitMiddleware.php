47af6743a9b26b067af99d0508be7af654ec79e2
mainbotan <ned.serafim@gmail.com>
2025-07-15 08:21:06 +0300

    RateLimitMiddleware

--- src/Http/RateLimiter/RateLimitMiddleware.php
+++ src/Http/RateLimiter/RateLimitMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace Rift\Core\Http\RateLimiter;
 
+use PHPUnit\Framework\Constraint\Operator;
 use Psr\Http\Message\ServerRequestInterface;
 use Rift\Contracts\Cache\CacheInterface;
 use Rift\Contracts\Middlewares\MiddlewareInterface;
@@ -22,13 +23,46 @@ class RateLimitMiddleware implements MiddlewareInterface {
             return Operation::success(null);
         }
         $limitData = $route['limit'];
+        $interval = (int) $limitData['interval'];
+        $maxAttempts = (int) $limitData['max_attempts'];
         $clientKey = $this->getClientKey($request, $limitData['strategy']);
+        $clientKeyForRoute = "{$clientKey}:{$route['path']}";
 
-        var_dump($clientKey);
-        var_dump($this->cache->get($clientKey));
-        return Operation::success(null);
+        return $this->cache->has($clientKeyForRoute)
+            ->catch(fn($errorObject) => $this->handleCacheError($errorObject))
+            ->then(function($isSaved) use ($clientKeyForRoute, $maxAttempts, $interval) {
+                if (!$isSaved) {
+                    if ($maxAttempts > 0) {
+                        return $this->cache->set($clientKeyForRoute, 1, $interval)
+                            ->catch(fn($errorObject) => $this->handleCacheError($errorObject))
+                            ->then(function() { 
+                                return Operation::success(null); 
+                            });
+                    }
+                    return Operation::error(Operation::HTTP_TOO_MANY_REQUESTS, "The request limit for the route has been exceeded. [{$maxAttempts}]");
+                }
+                return $this->cache->get($clientKeyForRoute)
+                    ->catch(fn($errorObject) => $this->handleCacheError($errorObject))
+                    ->then(function(int $requestsCount) use ($maxAttempts, $clientKeyForRoute) {
+                        if ($requestsCount+1 > $maxAttempts) {
+                            return Operation::error(Operation::HTTP_TOO_MANY_REQUESTS, "The request limit for the route has been exceeded. [{$maxAttempts}]");
+                        }
+                        
+                        return $this->cache->increment($clientKeyForRoute) 
+                             ->catch(fn($errorObject) => $this->handleCacheError($errorObject))
+                             ->then(function() { 
+                                    return Operation::success(null); 
+                                });
+                    });
+            });
     }
-
+    private function handleCacheError(OperationOutcome $errorObject): OperationOutcome {
+        return Operation::error(
+                Operation::HTTP_INTERNAL_SERVER_ERROR,
+                "Cache module error: {$errorObject->error}",
+                $errorObject->meta['debug']
+            );
+    }
     private function getClientKey(
         ServerRequestInterface $request, 
         string $strategy
